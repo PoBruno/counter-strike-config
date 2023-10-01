9f9e2bbc5feceab29e04fb97deef491291f4e829
Bruno Gomes <brunoshy@gmail.com>
2023-10-01 06:02:57 +0000

    script: ps1 automation lt cfg

---
+++ scripts/auto_cfg_lt.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest 'https://raw.githubusercontent.com/pobruno/counter-strike-config/main/autoexec.cfg' -OutFile ./autoexec.cfg
+Invoke-WebRequest 'https://raw.githubusercontent.com/pobruno/counter-strike-config/main/tre.cfg' -OutFile ./tre.cfg
