e56e5f7181208b2666256049406938e6e86935f9
Bruno Gomes <brunoshy@gmail.com>
2023-10-01 06:11:52 +0000

    script: ps1 automation lt cfg

--- scripts/auto_cfg_lt.ps1
+++ scripts/auto_cfg_lt.ps1
@@ -1,6 +1,8 @@
 clear
-Write-Host "Counter-Strike 2"
-$Path = Read-Host -Prompt 'Install Path:'
+Write-Host "Counter-Strike 2 - Auto Config Lorthe"
+Write-Host "-------------------------------------"
+Write-Host ""
+$Path = Read-Host -Prompt 'Install Path'
 cd $($Path)
 Invoke-WebRequest 'https://raw.githubusercontent.com/pobruno/counter-strike-config/main/autoexec.cfg' -OutFile ./autoexec.cfg
 Invoke-WebRequest 'https://raw.githubusercontent.com/pobruno/counter-strike-config/main/tre.cfg' -OutFile ./tre.cfg
