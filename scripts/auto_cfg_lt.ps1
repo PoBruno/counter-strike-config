130b007352e9c84c23188803e970671ee8492107
Bruno Gomes <brunoshy@gmail.com>
2023-10-05 01:32:58 -0300

    Update auto_cfg_lt.ps1
    adding dowload drag.cfg

--- scripts/auto_cfg_lt.ps1
+++ scripts/auto_cfg_lt.ps1
@@ -4,5 +4,9 @@ Write-Host "-------------------------------------"
 Write-Host ""
 $Path = Read-Host -Prompt 'Install Path'
 cd $($Path)
+Write-Host "Download Lorthe CFG"
 Invoke-WebRequest 'https://raw.githubusercontent.com/pobruno/counter-strike-config/main/autoexec.cfg' -OutFile ./autoexec.cfg
+Write-Host "Download DragãoManco CFG"
+Invoke-WebRequest 'https://raw.githubusercontent.com/pobruno/counter-strike-config/main/drag.cfg' -OutFile ./drag.cfg
+Write-Host "Download Treino CFG"
 Invoke-WebRequest 'https://raw.githubusercontent.com/pobruno/counter-strike-config/main/tre.cfg' -OutFile ./tre.cfg
